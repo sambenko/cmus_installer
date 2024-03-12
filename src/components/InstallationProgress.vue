5174c420273fd86cbfcea9efee694d22eab43c14
sambenko <sam.benko@gmail.com>
2024-03-12 14:42:18 +0100

    fixed component

--- src/components/InstallationProgress.vue
+++ src/components/InstallationProgress.vue
@@ -1,105 +1,146 @@
 <template>
-    <div class="installation-progress-container">
-      <div class="progress-section">
-        <div class="progress-bar-background">
-          <div v-if="isInstalling" class="progress-indicator" :style="{ width: `${progress}%` }"></div>
-        </div>
+  <div class="installation-progress-container">
+    <div class="progress-section">
+      <div class="progress-bar-background">
+        <div
+          v-if="isInstalling"
+          class="progress-indicator"
+          :style="{ width: `${progress}%` }"
+        ></div>
       </div>
-      <div class="output-section">
-        <p v-if="!isInstalling && logs.length === 0">Waiting for installation to start...</p>
-        <p v-for="(log, index) in logs" :key="index">{{ log }}</p>
-      </div>
-      <button class="cancel-button" @click="cancelInstallation" :class="{ clicked: cancelClicked }"
-        @animationend="cancelClicked = false">
+    </div>
+    <div class="output-section">
+      <p v-if="!isInstalling && logs.length === 0">
+        Waiting for installation to start...
+      </p>
+      <p v-for="(log, index) in logs" :key="index">{{ log }}</p>
+    </div>
+    <div class="button-container">
+      <button
+        v-show="isInstalling"
+        class="cancel-button"
+        @click="cancelInstallation"
+      >
         Cancel
       </button>
+      <button
+        v-show="!isInstalling"
+        class="finish-button"
+        @click="finishInstallation"
+      >
+        Finish
+      </button>
     </div>
-  </template>
+  </div>
+</template>
 
 <script>
-import { ref, onMounted } from 'vue';
-import { listen } from '@tauri-apps/api/event';
-import { invoke } from '@tauri-apps/api/tauri';
+import { ref, onMounted, nextTick } from "vue";
+import { listen } from "@tauri-apps/api/event";
+import { invoke } from "@tauri-apps/api/tauri";
+import { appWindow } from '@tauri-apps/api/window';
 
 export default {
-    name: 'InstallationProgress',
-    props: {
-        version: String,
-        path: String,
-    },
-    setup(props) {
-        console.log('Received props:', props);
-        const isInstalling = ref(false);
-        const logs = ref([]);
-        const progress = ref(0);
+  name: "InstallationProgress",
+  props: {
+    version: String,
+    path: String,
+  },
+  setup(props) {
+    console.log("Received props:", props);
+    const isInstalling = ref(false);
+    const logs = ref([]);
+    const progress = ref(0);
 
-        onMounted(() => {
-
-          listen("progress", (e) => {
-            logs.value.push(e.payload.message);
-          });
-
-            listen("download_progress", (e) => {
-              const percentage = e.payload.message;
-              progress.value = parseFloat(percentage);
-            });
-
-            listen('download-info', (event) => {
-                logs.value.push(event.payload.message);
-            });
-
-            listen('download-success', (event) => {
-                logs.value.push(event.payload.message);
-            });
-
-            listen('prg', (event) => {
-                logs.value.push(event.payload.message);
-            });
-
-            const { version, path } = props;
-            if (version && path) {
-            setTimeout(() => {
-                startInstallation(version, path);
-            }, 5000);
+    const scrollToBottom = () => {
+      nextTick(() => {
+        const outputSection = document.querySelector(".output-section");
+        if (outputSection) {
+          outputSection.scrollTop = outputSection.scrollHeight;
         }
+      });
+    };
+
+    onMounted(() => {
+      listen("progress", (e) => {
+        logs.value.push(e.payload.message);
+        scrollToBottom();
+      });
+
+      listen("download_progress", (e) => {
+        const percentage = e.payload.message;
+        progress.value = parseFloat(percentage);
+      });
+
+      listen("download-info", (event) => {
+        logs.value.push(event.payload.message);
+      });
+
+      listen("download-success", (event) => {
+        logs.value.push(event.payload.message);
+      });
+
+      listen("prg", (event) => {
+        logs.value.push(event.payload.message);
+      });
+
+      const { version, path } = props;
+      if (version && path) {
+        startInstallation(version, path);
+      }
+    });
+
+      const finishInstallation = async () => {
+      console.log("Finish clicked");
+      await appWindow.close();
+    };
+
+    async function startInstallation(version, path) {
+      isInstalling.value = true;
+      logs.value.push("Starting installation...");
+      let modified_version = version.substring(1);
+      try {
+        const downloadResult = await invoke("download_cmus", {
+          version: version,
+          targetPath: path,
         });
+        logs.value.push(downloadResult);
+        const decompressResult = await invoke("decompress", {
+          sourcePath: `${path}/cmus-${version}.zip`,
+          targetPath: path,
+        });
+        logs.value.push(decompressResult);
+        const installResult = await invoke("install_cmus", {
+          targetPath: `${path}/cmus-${modified_version}`,
+        });
+        logs.value.push(installResult);
+        scrollToBottom();
+      } catch (error) {
+        console.error("Installation failed:", error);
+        logs.value.push(`Installation failed: ${error}`);
+      }
 
-        async function startInstallation(version, path) {
-            isInstalling.value = true;
-            logs.value.push('Starting installation...');
-            try {
-                const downloadResult = await invoke('download_cmus', { version: version, targetPath: path });
-                logs.value.push(downloadResult);
-                const decompressResult = await invoke('decompress', { sourcePath: `${path}/cmus-${version}.zip`, targetPath: path });
-                logs.value.push(decompressResult);
-                const installResult = await invoke('install_cmus', { targetPath: path });
-                logs.value.push(installResult);
-            } catch (error) {
-                console.error('Installation failed:', error);
-                logs.value.push(`Installation failed: ${error}`);
-            }
+      isInstalling.value = false;
+    }
 
-            isInstalling.value = false;
-        }
-
-        return { isInstalling, logs, progress };
-    },
+    return { isInstalling, logs, progress, finishInstallation };
+  },
 };
 </script>
-
 
 <style scoped>
 .installation-progress-container {
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 10px;
   padding: 20px;
-  background-color: #333; /* Dark background */
+  background-color: #333;
   border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-  width: 80%;
-  margin: 50px auto;
-  color: #ccc; /* Light text color */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  width: 93%;
+  height: 93%;
+  position: relative;
+  overflow: hidden; /* Added */
 }
 
 .progress-section {
@@ -108,8 +149,8 @@ export default {
 
 .progress-bar-background {
   width: 100%;
-  height: 20px;
-  background-color: #555; /* Adjusted for dark theme */
+  height: 30px;
+  background-color: #555;
   border-radius: 10px;
   overflow: hidden;
 }
@@ -121,34 +162,50 @@ export default {
 }
 
 .output-section {
-  max-height: 200px;
   overflow-y: auto;
-  background-color: #444; /* Darker background for contrast */
+  background-color: #444;
   padding: 10px;
   border-radius: 8px;
-  border: 1px solid #666; /* Adjusted for dark theme */
+  border: 1px solid #666;
+  margin-bottom: 60px;
 }
 
 .output-section p {
   margin: 5px 0;
   font-family: monospace;
-  color: #ccc; /* Light text color for readability */
+  color: #ccc;
 }
 
+.button-container {
+  display: flex;
+  justify-content: flex-end;
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+}
+
+.finish-button,
 .cancel-button {
-    padding: 0.75rem 1.5rem;
+  padding: 0.75rem 1.5rem;
   font-size: 1.25rem;
   color: #ffffff;
-  background-color: #ff4d4d;
   border-radius: 8px;
   border: 1px solid transparent;
   transition: background-color 0.2s ease;
   cursor: pointer;
-  width: 30%;
+  width: 100px;
 }
 
+.finish-button {
+  background-color: #4caf50;
+}
+
+.finish-button:active,
 .cancel-button:active {
-  background-color: #e33e3e;
   transform: scale(0.95);
+}
+
+.cancel-button {
+  background-color: #ff4d4d;
 }
 </style>
